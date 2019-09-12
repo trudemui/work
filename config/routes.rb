4540a04270ae5bf3a07f0a8a98a98679e99c7e66
trudemui <alesth_130@yahoo.co.jp>
2019-09-12 16:53:05 +0900

    [add]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post 'posts' => 'posts#create'
   get 'posts' => 'posts#index'
   get 'posts/:id' => 'posts#show', as: 'post'
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
 end
